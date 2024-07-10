d8e47474871e29d714da68be09a06dac7e2171dd
MugishBeldar <mugishbeldar333@gmail.com>
2024-07-10 17:35:30 +0530

    Campare product page added

---
+++ app/compare/page.tsx
@@ -0,0 +1,11 @@
+import { CompareProductCarousel } from "@/components/user/carousel";
+
+const CompareProducPage = () => {
+  return (
+    <div className="w-full">
+      <CompareProductCarousel />
+    </div>
+  );
+};
+
+export default CompareProducPage;
--- store/store.ts
+++ store/store.ts
@@ -1,7 +1,16 @@
 import { create } from "zustand";
 import { createEcommerceAppSlice } from "./slices";
 import { EcommerceAppSliceTypes } from "./slices/ecommerce-app-slice";
+import { persist } from 'zustand/middleware'
 
-export const useAppStore = create<EcommerceAppSliceTypes>()((...a) => ({
+export const useAppStore = create<EcommerceAppSliceTypes>()(persist((...a) => ({
   ...createEcommerceAppSlice(...a),
-}));
+}),
+  {
+    name: 'ecommerce-app-slice',
+    partialize: (state) => ({
+      compareProduct: state.compareProduct
+    }),
+  }
+));
+
