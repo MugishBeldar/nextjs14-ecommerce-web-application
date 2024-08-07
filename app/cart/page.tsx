af28fd29667f4b0b64f72396286476e12c1cbbd5
MugishBeldar <mugishbeldar333@gmail.com>
2024-08-07 11:01:52 +0530

    Suspense added in cart

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,9 +1,16 @@
+import { Suspense } from 'react'
 import { Cart } from "@/components/user";
+
+function CartFallback() {
+  return <>Loading...</>
+}
 
 const CartPage = () => {
   return (
     <div className="  px-6 lg:container lg:px-0">
-      <Cart />
+      <Suspense fallback={<CartFallback />}>
+        <Cart />
+      </Suspense>
     </div>
   );
 };
